dcfed9281859e36f45789f677392cb79eafff6a7
NyleVII <stephenantymis@gmail.com>
2019-02-23 18:14:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+First time setup:
+    In the terminal run:
+        npm install
+
+
+Running server in dev mode:
+This will use nodemon module to automatically update server files dynamically so you don't have to constantly restart the server
+    In the terminal run:
+        npm run dev
+
+Running server in prod mode:
+    In the terminal run:
+        npm run start
---
+++ index.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const express = require('express');
+
+
+
+app.use(express.static("./"));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        nickChange = msg.split(" ");
+        if (msg === "") {
+            //Do nothing
+        }
+        else if (nickChange[0] === "/nick") {
+            socket.emit('nick change', nickChange[1]);
+        }
+        else {
+            io.emit('chat message', msg);
+        }
+
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
