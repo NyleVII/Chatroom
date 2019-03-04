a4a188b1945ca514925d2e342ac876fb5519481c
NyleVII <stephenantymis@gmail.com>
2019-03-04 11:55:55 -0700

    timestamps, user list updates, previous message log on connect

--- client.js
+++ client.js
@@ -1,6 +1,38 @@
 let userObj = {
 	uuid: "",
-	nickname: "Anonymous"
+	nickname: "Anonymous",
+	colour: "black"
+}
+
+class Message {
+    constructor(str, nickname, uuid, colour) {
+        this.text = str;
+        this.date = new Date();
+		this.nickname = nickname;
+		this.uuid = uuid;
+        this.colour = colour;
+    }
+}
+
+let dateTest;
+
+function prepChatMsg(msgText){
+	message = new Message(msgText, userObj.nickname, userObj.uuid, userObj.colour);
+	dateTest = message;
+	return message;
+}
+
+function getTimestamp(dateStr){
+	timestamp = "";
+	dateObj = new Date(dateStr);
+	timestamp += dateObj.getHours() + ":" + dateObj.getMinutes() + ":" +dateObj.getSeconds();
+	return timestamp;
+}
+
+function getChatMsg(msg){
+	chatline = "";
+	chatline += getTimestamp(msg.date) + " " + msg.nickname + ": " + msg.text;
+	return chatline;
 }
 
 $(document).ready(() => {
@@ -8,14 +40,22 @@ $(document).ready(() => {
 
 	$('form').submit(function (e) {
 		e.preventDefault(); // prevents page reloading
-		socket.emit('chat message', $('#m').val());
+		socket.emit('chat message', prepChatMsg($('#m').val()));
 		$('#m').val('');
 		return false;
 	});
 
 	socket.on('chat message', function (msg) {
-		$('#messages').append($('<li>').text(msg));
+		// $('#messages').append($('<li>').text(msg.text));
+		$('#messages').append($('<li>').text(getChatMsg(msg)));
 		$('#chat').scrollTop($("#chat")[0].scrollHeight);
+	});
+
+	socket.on('message log', function(messageHistory){
+		for(i = 0; i < messageHistory.length; i++){
+			$('#messages').append($('<li>').text(getChatMsg(messageHistory[i])));
+			$('#chat').scrollTop($("#chat")[0].scrollHeight);
+		}
 	});
 
 	socket.on('nick change', function (newNick) {
@@ -26,11 +66,14 @@ $(document).ready(() => {
 
 	//Update user list
 	socket.on('user update', function (userList){
+		console.log(userList);
+		$('#userlist').empty();
 		for(i = 0; i < userList.length; i++){
-			$('#users').append($('<li>').text(userList[i]));
+			$('#userlist').append($('<li>').text(userList[i]));
 		}
 	});
 
+	//Update this users id
 	socket.on("id update", function(newID){
 		userObj.uuid = newID;
 	});
--- index.js
+++ index.js
@@ -7,11 +7,26 @@ const uuid = require('uuid');
 class UserObj {
     constructor(uuid) {
         this.uuid = uuid;
-        this.name = "Anonymous";
+        this.nickname = "Anonymous";
+        this.colour = ""
     }
 }
 
+class Message {
+    constructor(str, nickname, uuid, colour) {
+        this.text = str;
+        this.date = new Date();
+		this.nickname = nickname;
+		this.uuid = uuid;
+        this.colour = colour;
+    }
+}
+
+//Dictionary of UserObj
 let userList = {};
+
+//array of Message
+let messageHistory = [];
 
 function updateUserNames(userList){
     let userNames = [];
@@ -24,12 +39,12 @@ function updateUserNames(userList){
             if(!obj.hasOwnProperty(prop)) continue;
     
             // your code
-            if(prop == "name"){
+            if(prop == "nickname"){
                 userNames.push(obj[prop]);
             }
         }
     }
-    console.log(userNames);
+    //console.log(userNames);
     return userNames;
 }
 
@@ -46,34 +61,60 @@ io.on('connection', function (socket) {
     socket.emit("id update", newID); //Send ID to user
     userObj = new UserObj(newID);
     userList[userObj.uuid] = userObj;
-    console.log(userList);
+    //console.log(userList);
+    //Send newly connected user the userlist
     io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
+    socket.emit('message log', messageHistory);
 
 	//When user disconnects
     socket.on('disconnect', function () {
         console.log('user disconnected');
+        console.log('Deleting user: ' + newID);
         delete userList.newID;
-        console.log(userList);
+        //console.log(userList);
         io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
     });
-});
 
-io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
-        nickChange = msg.split(" ");
-        if (msg === "") {
+        console.log("Incoming message: " + msg.date);
+        nickChange = msg.text.split(" ");
+        if (msg.text === "") {
             //Do nothing
         }
         else if (nickChange[0] === "/nick") {
             socket.emit('nick change', nickChange[1]);
+            userList[msg.uuid].nickname = nickChange[1];
             io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
         }
         else {
+            messageHistory.push(msg);
             io.emit('chat message', msg);
         }
 
     });
+
 });
+
+// io.on('connection', function (socket) {
+//     console.log("doesthisrun?");
+//     socket.on('chat message', function (msg) {
+//         console.log("Incoming message: " + msg.date);
+//         nickChange = msg.text.split(" ");
+//         if (msg.text === "") {
+//             //Do nothing
+//         }
+//         else if (nickChange[0] === "/nick") {
+//             socket.emit('nick change', nickChange[1]);
+//             userList[msg.uuid].nickname = nickChange[1];
+//             io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
+//         }
+//         else {
+//             messageHistory.push(msg);
+//             io.emit('chat message', msg);
+//         }
+
+//     });
+// });
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
