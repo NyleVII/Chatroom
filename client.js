1c26e8e3d2a72c6873ddb5b3585e9f857cac7c37
NyleVII <stephenantymis@gmail.com>
2019-03-04 16:26:49 -0700

    Cookie updates? WIP

--- client.js
+++ client.js
@@ -1,43 +1,85 @@
 let userObj = {
-	uuid: "",
-	nickname: "Anonymous",
-	colour: "black"
+	uuid: getCookie("uuid"),
+	nickname: getCookie("nickname"),
+	colour: getCookie("nickColour")
 }
 
 class Message {
-    constructor(str, nickname, uuid, colour) {
-        this.text = str;
-        this.date = new Date();
+	constructor(str, nickname, uuid, colour) {
+		this.text = str;
+		this.date = new Date();
 		this.nickname = nickname;
 		this.uuid = uuid;
-        this.colour = colour;
-    }
+		this.colour = colour;
+	}
 }
 
-let dateTest;
+//code from W3 schools modified for own use
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-function prepChatMsg(msgText){
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+// function checkCookie() {
+// 	var user = getCookie("nickname");
+// 	if (user != "") {
+// 		//Do something when we have a username already
+// 	} else {
+// 		user = prompt("Please enter your name:", "");
+// 		if (user != "" && user != null) {
+// 			setCookie("nickname", user, 30);
+// 		}
+// 	}
+// }
+
+
+function prepChatMsg(msgText) {
 	message = new Message(msgText, userObj.nickname, userObj.uuid, userObj.colour);
 	dateTest = message;
 	return message;
 }
 
-function getTimestamp(dateStr){
+function getTimestamp(dateStr) {
 	timestamp = "";
 	dateObj = new Date(dateStr);
-	timestamp += dateObj.getHours() + ":" + dateObj.getMinutes() + ":" +dateObj.getSeconds();
+	timestamp += dateObj.getHours() + ":" + dateObj.getMinutes() + ":" + dateObj.getSeconds();
 	return timestamp;
 }
 
-function getChatMsg(msg){
+function getChatMsg(msg) {
 	chatline = "";
-	chatline += getTimestamp(msg.date) + " " + msg.nickname + ": " + msg.text;
+	chatline += getTimestamp(msg.date) + " " + "<span style='color:#" + msg.colour + "'>" + msg.nickname + "</span>" + ": " + msg.text;
 	return chatline;
 }
 
 $(document).ready(() => {
 	var socket = io();
 
+	//Check cookie on load to initialize
+	//checkCookie();
+
+	socket.on("request userObj", function(newID){
+		socket.emit("send userObj", userObj);
+	});
+
+	//emit chat message
 	$('form').submit(function (e) {
 		e.preventDefault(); // prevents page reloading
 		socket.emit('chat message', prepChatMsg($('#m').val()));
@@ -47,13 +89,13 @@ $(document).ready(() => {
 
 	socket.on('chat message', function (msg) {
 		// $('#messages').append($('<li>').text(msg.text));
-		$('#messages').append($('<li>').text(getChatMsg(msg)));
+		$('#messages').append($('<li>').html(getChatMsg(msg)));
 		$('#chat').scrollTop($("#chat")[0].scrollHeight);
 	});
 
-	socket.on('message log', function(messageHistory){
-		for(i = 0; i < messageHistory.length; i++){
-			$('#messages').append($('<li>').text(getChatMsg(messageHistory[i])));
+	socket.on('message log', function (messageHistory) {
+		for (i = 0; i < messageHistory.length; i++) {
+			$('#messages').append($('<li>').html(getChatMsg(messageHistory[i])));
 			$('#chat').scrollTop($("#chat")[0].scrollHeight);
 		}
 	});
@@ -62,19 +104,24 @@ $(document).ready(() => {
 		userObj.nickname = newNick;
 		console.log("Nick changed to: " + userObj.nickname);
 		$('#nav').text("Welcome " + userObj.nickname);
+		setCookie("nickname", newNick, 30);
+	});
+
+	socket.on('nick colour', function (newColour) {
+		userObj.colour = newColour;
+		setCookie("nickColour", newColour, 30);
 	});
 
 	//Update user list
-	socket.on('user update', function (userList){
-		console.log(userList);
+	socket.on('user update', function (userList) {
 		$('#userlist').empty();
-		for(i = 0; i < userList.length; i++){
-			$('#userlist').append($('<li>').text(userList[i]));
+		for (i = 0; i < userList.length; i++) {
+			$('#userlist').append($('<li>').html("<span style='color:#" + userList[i].colour + "'>" + userList[i].nickname + "</span>"));
 		}
 	});
 
 	//Update this users id
-	socket.on("id update", function(newID){
+	socket.on("id update", function (newID) {
 		userObj.uuid = newID;
 	});
 
--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const express = require('express');
 const uuid = require('uuid');
 
 class UserObj {
-    constructor(uuid) {
+    constructor(uuid, nick) {
         this.uuid = uuid;
-        this.nickname = "Anonymous";
-        this.colour = ""
+        this.nickname = nick;
+        this.colour = "FFFFFF"
     }
 }
 
@@ -16,8 +16,8 @@ class Message {
     constructor(str, nickname, uuid, colour) {
         this.text = str;
         this.date = new Date();
-		this.nickname = nickname;
-		this.uuid = uuid;
+        this.nickname = nickname;
+        this.uuid = uuid;
         this.colour = colour;
     }
 }
@@ -25,22 +25,32 @@ class Message {
 //Dictionary of UserObj
 let userList = {};
 
+let userCount = 0;
+
 //array of Message
 let messageHistory = [];
 
-function updateUserNames(userList){
+function getNewNick() {
+    userCount += 1;
+    return "User" + userCount;
+}
+
+function updateUserNames(userList) {
     let userNames = [];
 
     for (var key in userList) {
-
         var obj = userList[key];
         for (var prop in obj) {
             // skip loop if the property is from prototype
-            if(!obj.hasOwnProperty(prop)) continue;
-    
-            // your code
-            if(prop == "nickname"){
-                userNames.push(obj[prop]);
+            if (!obj.hasOwnProperty(prop)) continue;
+
+            // compare to nickname to only push it once
+            if (prop == "nickname") {
+                tempObj = {};
+                tempObj.nickname = obj.nickname;
+                tempObj.colour = obj.colour
+                userNames.push(tempObj);
+                // userNames.push(obj[prop]);
             }
         }
     }
@@ -55,28 +65,34 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-	console.log('a user connected');
-    
-    var newID = uuid.v4();
+    console.log('a user connected');
+
+    let newID = uuid.v4();
     socket.emit("id update", newID); //Send ID to user
-    userObj = new UserObj(newID);
+    userObj = new UserObj(newID, getNewNick());
     userList[userObj.uuid] = userObj;
     //console.log(userList);
     //Send newly connected user the userlist
     io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
     socket.emit('message log', messageHistory);
 
-	//When user disconnects
+
+
+    
+    //Send newly connected user the userlist
+
+    socket.emit('message log', messageHistory);
+
+    //When user disconnects
     socket.on('disconnect', function () {
         console.log('user disconnected');
         console.log('Deleting user: ' + newID);
-        delete userList.newID;
+        delete userList[newID];
         //console.log(userList);
         io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
     });
 
     socket.on('chat message', function (msg) {
-        console.log("Incoming message: " + msg.date);
         nickChange = msg.text.split(" ");
         if (msg.text === "") {
             //Do nothing
@@ -84,6 +100,12 @@ io.on('connection', function (socket) {
         else if (nickChange[0] === "/nick") {
             socket.emit('nick change', nickChange[1]);
             userList[msg.uuid].nickname = nickChange[1];
+            io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
+        }
+        else if (nickChange[0] === "/nickcolor" || nickChange[0] === "/nickcolour") {
+            userList[msg.uuid].colour = nickChange[1];
+            console.log(userList[msg.uuid].colour);
+            socket.emit('nick colour', nickChange[1]);
             io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
         }
         else {
@@ -95,26 +117,7 @@ io.on('connection', function (socket) {
 
 });
 
-// io.on('connection', function (socket) {
-//     console.log("doesthisrun?");
-//     socket.on('chat message', function (msg) {
-//         console.log("Incoming message: " + msg.date);
-//         nickChange = msg.text.split(" ");
-//         if (msg.text === "") {
-//             //Do nothing
-//         }
-//         else if (nickChange[0] === "/nick") {
-//             socket.emit('nick change', nickChange[1]);
-//             userList[msg.uuid].nickname = nickChange[1];
-//             io.emit('user update', updateUserNames(userList)); //Send userList to all connected users
-//         }
-//         else {
-//             messageHistory.push(msg);
-//             io.emit('chat message', msg);
-//         }
 
-//     });
-// });
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
